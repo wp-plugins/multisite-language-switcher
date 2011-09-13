18b070dbbd647f651c62b85864b04b7576ea7a74
realloc <realloc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-13 16:55:26 +0000

    git-svn-id: https://plugins.svn.wordpress.org/multisite-language-switcher/trunk@437562 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/MslsCustomColumn.php
+++ includes/MslsCustomColumn.php
@@ -19,7 +19,7 @@ class MslsCustomColumn extends MslsMain implements IMslsMain {
             add_action( 'manage_pages_custom_column' , array( $obj, 'pages_columns' ), 10, 2 );
             add_action( 'manage_posts_custom_column' , array( $obj, 'posts_columns' ), 10, 2 );
             add_action( 'manage_category_custom_column' , array( $obj, 'category_columns' ), 10, 3 );
-            add_action( 'manage_post_tag_custom_column' , array( $obj, 'post_tag_columns' ), 10, 3s );
+            add_action( 'manage_post_tag_custom_column' , array( $obj, 'post_tag_columns' ), 10, 3 );
         }
     }
 
