8cd60b468438c718c172407db30c5bc3ae91e7e0
realloc <realloc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-03 07:57:22 +0000

    Tagging version 0.9.8
    git-svn-id: https://plugins.svn.wordpress.org/multisite-language-switcher/tags/0.9.8@593891 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/MslsOptionsQueryYear.php
+++ includes/MslsOptionsQueryYear.php
@@ -17,11 +17,12 @@ class MslsOptionsQueryYear extends MslsOptionsQuery {
     public function has_value( $language ) {
         if ( !isset( $this->arr[$language] ) ) {
             global $wpdb;
-            $sql                  = sprintf(
-                "SELECT count(ID) FROM {$wpdb->posts} WHERE YEAR(post_date) = %d AND post_status = 'publish'",
-                (int) $this->args[0]
+            $this->arr[$language] = $wpdb->get_var(
+                $wpdb->prepare(
+                    "SELECT count(ID) FROM {$wpdb->posts} WHERE YEAR(post_date) = %d AND post_status = 'publish'",
+                    $this->args[0]
+                )
             );
-            $this->arr[$language] = $wpdb->get_var( $sql );
         }
         return (bool) $this->arr[$language];
     }
